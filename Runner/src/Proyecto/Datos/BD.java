4dabbb6c8c20f9f89bbd4415d8cd60a3777bf2de
yeray <yeraybellanco@opendeusto.es>
2018-11-23 14:45:28 +0100

    BD

--- Runner/src/Proyecto/Datos/BD.java
+++
@@ -1,9 +0,0 @@
-package Proyecto.Datos;
-
-/**
- * BD del runner
- *
- */
-public class BD {
-
-}
---
+++ src/Proyecto/Datos/BD.java
@@ -0,0 +1,13 @@
+package Proyecto.Datos;
+
+/**
+ * BD del runner
+ *
+ */
+public class BD {
+	
+	public static void main(String[] args) {
+		System.out.println("hola");
+	}
+
+}
